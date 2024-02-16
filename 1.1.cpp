aa03f1323bdaec7bd667e6983f0cc4722d359f62
uladikakubovic <160218919+uladikakubovic@users.noreply.github.com>
2024-02-16 11:53:29 +0300

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "1.2.h"
+
+
+
+int main()
+{
+	system("chcp 1251");
+	enter();
+	print();
+	find_names_starting_with_A();
+	return 0;
+}
